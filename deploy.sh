c63eb8ee8243aa09fe7e05874e87a788ee570e24
Tomáš Věžník <veznitom@gmail.com>
2025-09-08 17:46:14 +0200

    most of the style done

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+
+# ------------------------------------------------------------------------------------
+# Hyprland configs
+# ------------------------------------------------------------------------------------
+mkdir -p ~/.config/hypr
 cp -r ./config/hypr/hyprland.conf ~/.config/hypr/
 cp -r ./config/hypr/hyprpaper.conf ~/.config/hypr/
 cp -r ./config/hypr/load_wallpaper.sh ~/.config/hypr/
+cp -r ./config/hypr/wallpapers ~/.config/wallpapers
 
-if [[ $hostname -eq "Note" ]]; then
-cp -r ./config/hypr/monitors-notebook.conf ~/.config/hypr/monitors.conf
-fi
+case $HOSTNAME in
+  ("Note") cp -r ./config/hypr/monitors-notebook.conf ~/.config/hypr/monitors.conf;;
+  ("PrahaPC") cp -r ./config/hypr/monitors-desktop.conf ~/.config/hypr/monitors.conf;;
+  (*) cp -r ./config/hypr/monitors-desktop.conf ~/.config/hypr/monitors.conf;;
+esac
+# ------------------------------------------------------------------------------------
+# Other configs
+# ------------------------------------------------------------------------------------
+cp -r ./config/waybar ~/.config/
+cp -r ./config/kitty ~/.config/
+cp -r ./config/wlogout ~/.config/
+cp -r ./config/rofi ~/.config/
