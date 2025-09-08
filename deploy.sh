e067de5c4eef43657a3c75466a9cfea590a51ec9
Tomáš Věžník <veznitom@gmail.com>
2025-09-09 01:44:14 +0200

    total overhaul

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,12 @@
 # Hyprland configs
 # ------------------------------------------------------------------------------------
 mkdir -p ~/.config/hypr
-cp -r ./config/hypr/hyprland.conf ~/.config/hypr/
+cp -r ./config/hypr/hyprland.conf  ~/.config/hypr/
 cp -r ./config/hypr/hyprpaper.conf ~/.config/hypr/
-cp -r ./config/hypr/load_wallpaper.sh ~/.config/hypr/
-cp -r ./config/hypr/wallpapers ~/.config/wallpapers
-
+cp -r ./config/hypr/hyprlock.conf  ~/.config/hypr/
+cp -r ./config/hypr/hypridle.conf  ~/.config/hypr/
+cp -r ./config/hypr/wallpapers     ~/.config/hypr/
+cp -r ./config/hypr/colors.conf    ~/.config/hypr/
 case $HOSTNAME in
   ("Note") cp -r ./config/hypr/monitors-notebook.conf ~/.config/hypr/monitors.conf;;
   ("PrahaPC") cp -r ./config/hypr/monitors-desktop.conf ~/.config/hypr/monitors.conf;;
@@ -19,5 +20,7 @@ esac
 # ------------------------------------------------------------------------------------
 cp -r ./config/waybar ~/.config/
 cp -r ./config/kitty ~/.config/
-cp -r ./config/wlogout ~/.config/
+cp -r ./config/nwg-bar ~/.config/
 cp -r ./config/rofi ~/.config/
+
+chmod +x ~/.config/waybar/launch.sh
