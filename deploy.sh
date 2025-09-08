f7193205bae0711d59c88454fa43f845c3a3d14e
Tomáš Věžník <veznitom@gmail.com>
2025-09-08 11:54:13 +0200

    monitor PC dependent setup

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-cp -r ./.config/* ~/.config/
+cp -r ./config/hypr/hyprland.conf ~/.config/hypr/
+cp -r ./config/hypr/hyprpaper.conf ~/.config/hypr/
+cp -r ./config/hypr/load_wallpaper.sh ~/.config/hypr/
+
+if [[ $hostname -eq "Note" ]]; then
+cp -r ./config/hypr/monitors-notebook.conf ~/.config/hypr/monitors.conf
+fi
