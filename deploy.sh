df06db4d40473c17e833b18a0e25f81d9d39fd4f
Tomáš Věžník <veznitom@gmail.com>
2025-09-09 14:18:43 +0200

    rofi style, deploy,kitty,... update

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
-
+CONF_DIR="/home/liz/TMP/dotfiles"
 # ------------------------------------------------------------------------------------
 # Hyprland configs
 # ------------------------------------------------------------------------------------
 mkdir -p ~/.config/hypr
-cp -r ./config/hypr/hyprland.conf  ~/.config/hypr/
-cp -r ./config/hypr/hyprpaper.conf ~/.config/hypr/
-cp -r ./config/hypr/hyprlock.conf  ~/.config/hypr/
-cp -r ./config/hypr/hypridle.conf  ~/.config/hypr/
-cp -r ./config/hypr/wallpapers     ~/.config/hypr/
-cp -r ./config/hypr/colors.conf    ~/.config/hypr/
+cp -r $CONF_DIR/config/hypr/hyprland.conf  ~/.config/hypr/
+cp -r $CONF_DIR/config/hypr/hyprpaper.conf ~/.config/hypr/
+cp -r $CONF_DIR/config/hypr/hyprlock.conf  ~/.config/hypr/
+cp -r $CONF_DIR/config/hypr/hypridle.conf  ~/.config/hypr/
+cp -r $CONF_DIR/config/hypr/wallpapers     ~/.config/hypr/
+cp -r $CONF_DIR/config/hypr/colors.conf    ~/.config/hypr/
 case $HOSTNAME in
-  ("Note") cp -r ./config/hypr/monitors-notebook.conf ~/.config/hypr/monitors.conf;;
-  ("PrahaPC") cp -r ./config/hypr/monitors-desktop.conf ~/.config/hypr/monitors.conf;;
-  (*) cp -r ./config/hypr/monitors-desktop.conf ~/.config/hypr/monitors.conf;;
+  ("Note") cp -r    $CONF_DIR/config/hypr/monitors-notebook.conf ~/.config/hypr/monitors.conf;;
+  ("PrahaPC") cp -r $CONF_DIR/config/hypr/monitors-desktop.conf  ~/.config/hypr/monitors.conf;;
+  (*) cp -r         $CONF_DIR/config/hypr/monitors-desktop.conf  ~/.config/hypr/monitors.conf;;
 esac
 # ------------------------------------------------------------------------------------
 # Other configs
 # ------------------------------------------------------------------------------------
-cp -r ./config/waybar ~/.config/
-cp -r ./config/kitty ~/.config/
-cp -r ./config/nwg-bar ~/.config/
-cp -r ./config/rofi ~/.config/
+cp -r $CONF_DIR/config/waybar  ~/.config/
+cp -r $CONF_DIR/config/kitty   ~/.config/
+cp -r $CONF_DIR/config/nwg-bar ~/.config/
+cp -r $CONF_DIR/config/rofi    ~/.config/
+mkdir -p ~/.local/share/rofi/themes
+cp $CONF_DIR/config/rofi/Breeze-Dark.rasi ~/.local/share/rofi/themes/
 
 chmod +x ~/.config/waybar/launch.sh
+
